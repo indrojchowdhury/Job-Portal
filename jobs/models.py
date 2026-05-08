24f38b3b57192ad1206905cbda3bce942738562b
indrojchowdhury <indrojchowdhurys@mail.com>
2026-05-08 13:06:54 +0600

    Added Job and Application models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,26 @@
+# jobs/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Job(models.Model):
+    # Linking the job to the user who created it (Employer)
+    employer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='jobs')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    salary = models.CharField(max_length=50, blank=True, null=True)
+    company_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Application(models.Model):
+    # Linking the job and the applicant (Seeker)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applications')
+    applicant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='applied_jobs')
+    resume_link = models.URLField(max_length=500, blank=True, null=True) # Simple link for resume
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.applicant.username} applied for {self.job.title}"
---
+++ jobs/urls.py
@@ -0,0 +1,7 @@
+# jobs/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # We will add paths like 'list/', 'create/', etc. very soon
+]
