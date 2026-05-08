df5e779185cd100b2b6f9d32614fb9c2ad4a935d
indrojchowdhury <indrojchowdhurys@mail.com>
2026-05-08 13:22:52 +0600

    Setup Job models, Base template, and static CSS configuration

--- Job_Portal/settings.py
+++ Job_Portal/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Job_Portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Global templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,8 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # We will add paths like 'list/', 'create/', etc. very soon
+    path('', views.job_list, name='job_list'),
 ]
